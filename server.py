0780f76017ac339e3adbf49d0525788fdc13174f
Damian Ryś <55887851+Damiry0@users.noreply.github.com>
2022-04-25 00:17:07 +0200

    Changed script to function

--- server.py
+++ server.py
@@ -5,41 +5,44 @@ from PIL import Image
 import matplotlib.pyplot as pl
 from subprocess import Popen
 
-server_socket = socket.socket()
-server_socket.bind(('', 8000))  # ADD IP HERE
-server_socket.listen(0)
-p = Popen(['sh', './login.sh'])
 
-# Accept a single connection and make a file-like object out of it
-connection = server_socket.accept()[0].makefile('rb')
-try:
-    img = None
-    while True:
-        # Read the length of the image as a 32-bit unsigned int. If the
-        # length is zero, quit the loop
-        image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
-        if not image_len:
-            break
-        # Construct a stream to hold the image data and read the image
-        # data from the connection
-        image_stream = io.BytesIO()
-        image_stream.write(connection.read(image_len))
-        # Rewind the stream, open it as an image with PIL and do some
-        # processing on it
-        image_stream.seek(0)
-        image = Image.open(image_stream)
+def run():
+    server_socket = socket.socket()
+    server_socket.bind(('', 8000))  # ADD IP HERE
+    server_socket.listen(0)
+    p = Popen(['sh', './login.sh'])
 
-        if img is None:
-            img = pl.imshow(image)
-        else:
-            img.set_data(image)
+    # Accept a single connection and make a file-like object out of it
+    connection = server_socket.accept()[0].makefile('rb')
+    try:
+        img = None
+        while True:
+            # Read the length of the image as a 32-bit unsigned int. If the
+            # length is zero, quit the loop
+            image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
+            if not image_len:
+                break
+            # Construct a stream to hold the image data and read the image
+            # data from the connection
+            image_stream = io.BytesIO()
+            image_stream.write(connection.read(image_len))
+            # Rewind the stream, open it as an image with PIL and do some
+            # processing on it
+            image_stream.seek(0)
+            image = Image.open(image_stream)
 
-        pl.pause(0.01)
-        pl.draw()
+            if img is None:
+                img = pl.imshow(image)
+            else:
+                img.set_data(image)
 
-        print('Image is %dx%d' % image.size)
-        image.verify()
-        print('Image is verified')
-finally:
-    connection.close()
-    server_socket.close()
+            pl.pause(0.01)
+            pl.draw()
+
+            print('Image is %dx%d' % image.size)
+            # image.verify()
+            # print('Image is verified')
+            return image
+    finally:
+        connection.close()
+        server_socket.close()
