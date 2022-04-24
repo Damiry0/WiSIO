45856ff7d9e6a89fdcfe4330389f63dbf826caf4
Damian Ryś <55887851+Damiry0@users.noreply.github.com>
2022-04-24 23:11:26 +0200

    Changed camera settings

--- client.py
+++ client.py
@@ -5,9 +5,7 @@ import time
 import picamera
 
 client_socket = socket.socket()
-
 client_socket.connect(('192.168.1.163', 8000))  # ADD IP HERE
-
 # Make a file-like object out of the connection
 connection = client_socket.makefile('wb')
 try:
@@ -22,22 +20,19 @@ try:
     # temporarily (we could write it directly to connection but in this
     # case we want to find out the size of each capture first to keep
     # our protocol simple)
-    start = time.time()
     stream = io.BytesIO()
-    for foo in camera.capture_continuous(stream, 'jpeg'):
+    camera.capture(stream,'png')
         # Write the length of the capture to the stream and flush to
         # ensure it actually gets sent
-        connection.write(struct.pack('<L', stream.tell()))
-        connection.flush()
+    connection.write(struct.pack('<L', stream.tell()))
+    connection.flush()
         # Rewind the stream and send the image data over the wire
-        stream.seek(0)
-        connection.write(stream.read())
-        # If we've been capturing for more than 30 seconds, quit
-        if time.time() - start > 60:
-            break
+    stream.seek(0)
+    connection.write(stream.read())
         # Reset the stream for the next capture
-        stream.seek(0)
-        stream.truncate()
+    stream.seek(0)
+    stream.truncate()
+    print("tutaj")
     # Write a length of zero to the stream to signal we're done
     connection.write(struct.pack('<L', 0))
 finally:
