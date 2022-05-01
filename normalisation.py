a562a716de8e9f052f3c0ec4f661bd4450308b04
JakubNowek <jakubbnwek@gmail.com>
2022-05-01 12:17:34 +0200

    broke normalization into 2 .py and added template matching?

--- normalisation.py
+++ normalisation.py
@@ -15,16 +15,4 @@
 
 # https://emrecankuran.medium.com/a-guide-to-contrast-enhancement-transformation-functions-histogram-sliding-contrast-stretching-34149e5cdeed
 
-import numpy as np
-import cv2
 
-img = cv2.imread('boards/1good.jpg', 1)
-
-median = cv2.medianBlur(img, 13)
-compare = np.concatenate((img, median), axis=1) #side by side comparison
-
-cv2.imshow('img', compare)
-cv2.waitKey(0)
-cv2.destroyAllWindows
-
-cv2.imwrite('boards/1res.jpg', compare)
---
+++ template_matching.py
@@ -0,0 +1,38 @@
+# to mamy 6 metod template matchingu do wykrywania elementu template na podanym zdjęciu img.
+# nie wiem, czy zwykłe szukanie różnic wypali, więc możnaby:
+# - podzielić obrazek wzorcowy na prostokąty i w obrazku wejściowym szukać prostokątów ze wzorca
+# - jeśli template został znaleziony w danym miejscu, to zamalowujemy ten obszar na czarno
+# - kiedy sprawdziliśmy wszystkie prostokąty, to teoretycznie wszystkie "nieczarne" obszary powinny być obszarami,
+# na których są różnice
+
+
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+color = (0, 255, 0)
+img = cv.imread('boards/3defects.jpg',0)
+img2 = img.copy()
+template = cv.imread('boards/part.jpg',0)
+w, h = template.shape[::-1]
+# All the 6 methods for comparison in a list
+methods = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
+            'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
+for meth in methods:
+    img = img2.copy()
+    method = eval(meth)
+    # Apply template Matching
+    res = cv.matchTemplate(img,template,method)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+    if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
+        top_left = min_loc
+    else:
+        top_left = max_loc
+    bottom_right = (top_left[0] + w, top_left[1] + h)
+    cv.rectangle(img,top_left, bottom_right, 255, -1)
+    plt.subplot(121),plt.imshow(res,cmap='gray')
+    plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122),plt.imshow(img,cmap='gray')
+    plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+    plt.suptitle(meth)
+    plt.show()
