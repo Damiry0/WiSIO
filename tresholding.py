9ab4bd23ad889bb697b07d686ca38e2747a0a778
JakubNowek <jakubbnwek@gmail.com>
2022-06-02 13:27:03 +0200

    adding comments, commenting unnecessary lines

--- tresholding.py
+++ tresholding.py
@@ -7,15 +7,13 @@ import shutil
 
 
 # Change the working directory to the folder this script is in.
-# Doing this because I'll be putting the files from each video in their own folder on GitHub
-# os.chdir(os.path.dirname(os.path.abspath(__file__)))
 os.chdir('boards')
 
 # checking if there is an output folder to stash the tiles in, if not, creating one
 if not os.path.isdir('output'):
     os.mkdir('output')
 else:
-    # removing files from output, to make place for
+    # removing files from output if there is any
     for path in os.scandir('output/'):
         if path.is_file():
             os.remove(path)
@@ -75,18 +73,18 @@ def check_adjacent(w_tile, h_tile, x_offset_1, x_offset_2, y_offset_1, y_offset_
     :return: a list of neighbour tile flags
     """
     flag_adjacent = []
-    if (abs(x_offset_1 - x_offset_2) / w_tile == 1.0 and y_offset_1 == y_offset_2):
+    if abs(x_offset_1 - x_offset_2) / w_tile == 1.0 and y_offset_1 == y_offset_2:
         flag_adjacent.append('1')
-        if (x_offset_1 < x_offset_2):
+        if x_offset_1 < x_offset_2:
             flag_adjacent.append('R')
         else:
             flag_adjacent.append('L')
     else:
         flag_adjacent.append('0')
 
-    if (abs(y_offset_1 - y_offset_2) / h_tile == 1.0 and x_offset_1 == x_offset_2):
+    if abs(y_offset_1 - y_offset_2) / h_tile == 1.0 and x_offset_1 == x_offset_2:
         flag_adjacent.append('1')
-        if (y_offset_1 < y_offset_2):
+        if y_offset_1 < y_offset_2:
             flag_adjacent.append('B')
         else:
             flag_adjacent.append('T')
@@ -94,6 +92,7 @@ def check_adjacent(w_tile, h_tile, x_offset_1, x_offset_2, y_offset_1, y_offset_
         flag_adjacent.append('0')
 
     return flag_adjacent
+
 
 def needle_in_hay_stack(haystack_name, number_of_photos, list_of_tiles, threshold=0.07):
     """
@@ -124,22 +123,22 @@ def needle_in_hay_stack(haystack_name, number_of_photos, list_of_tiles, threshol
 
         if locations1:
             # Getting index of most similar image
-            location_index = list2.index(min(list2))
-            location = locations1[location_index]
-            print('Found needle.')
-            needle_w = needle_img.shape[1]
-            needle_h = needle_img.shape[0]
-            line_color = (120, 120, 0)
-            line_type = cv.LINE_4
+            # location_index = list2.index(min(list2))
+            # location = locations1[location_index]
+            # print('Found needle.')
+            # needle_w = needle_img.shape[1]
+            # needle_h = needle_img.shape[0]
+            # line_color = (120, 120, 0)
+            # line_type = cv.LINE_4
             # Drawing a rectangle
             # Determine the box positions
-            top_left = location
-            bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
+            # top_left = location
+            # bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
             # Draw the box
-            cv.rectangle(haystack_img, top_left, bottom_right, line_color, line_type)
-            #res = cv.resize(haystack_img, (960, 540))
-            #cv.imshow('Matches', res)
-            #cv.waitKey()
+            # cv.rectangle(haystack_img, top_left, bottom_right, line_color, line_type)
+            # res = cv.resize(haystack_img, (960, 540))
+            # cv.imshow('Matches', res)
+            # cv.waitKey()
             list_of_tiles.remove(list_of_bundles[i])
         else:
             print('Needle not found.')
@@ -147,18 +146,23 @@ def needle_in_hay_stack(haystack_name, number_of_photos, list_of_tiles, threshol
 
 # list of errors to display
 list_of_frames = []
-
+# list of error subframes
 deep_list_of_frames = []
+# good image entered by user
+source_image = "dobra_wycieta.png"     # USER PARAM
+# bad image entered by user
+input_image = "zla_wycieta.png"     # USER PARAM
+
 
 '''Dividing into tiles'''
-number_of_tiles = tile("dobra_wycieta.png", "output/", list_of_frames, div_w=2, div_h=2)
+number_of_tiles = tile(source_image, "output/", list_of_frames, div_w=2, div_h=2)
 print(range(number_of_tiles))
 # list of images to delete
 list_of_all_frames = list_of_frames.copy()
 
 '''Searching for every element from "output" in input frame'''
 print('Dlugosc przed:', len(list_of_frames))
-needle_in_hay_stack('zla_wycieta.png', number_of_tiles, list_of_frames, threshold=0.02)
+needle_in_hay_stack(input_image, number_of_tiles, list_of_frames, threshold=0.02)
 print('Dlugosc po', len(list_of_frames))
 
 # All frames - not_needle frames = list of needle frames that should be erased from "output"
@@ -184,15 +188,13 @@ for i in range(howDeep):
     for frame in list_of_frames:
         number_of_tiles = tile(frame.tilefname, "output_temp/", deep_list_of_frames, div_w=2, div_h=2, offset=frame.offset)
         list_of_all_frames = deep_list_of_frames.copy()
-        needle_in_hay_stack('zla_wycieta.png', number_of_tiles, deep_list_of_frames, threshold=thr)
+        needle_in_hay_stack(input_image, number_of_tiles, deep_list_of_frames, threshold=thr)
         list_to_erase = [not_needle for not_needle in list_of_all_frames if not_needle not in deep_list_of_frames]
-        # removing found frames from output_temp/ - tu się wywala przy kolejnym powtórzeniu, bo usuwa płytki z
-        # output, zamiast output_temp
+
         for item in list_to_erase:
             os.remove(item.tilefname)
 
     # PUTTING subFRAMES INTO OUTPUT AND CHANGING list_of_frames - now subframes become frames
-
     # removing files from output, to make place for
     for path in os.scandir('output/'):
         if path.is_file():
@@ -207,7 +209,6 @@ for i in range(howDeep):
         item.tilefname = 'output/' + item.tilefname.split('/')[-1]
         print(item.tilefname)
 
-    # #to niżej nie zadziała chyba tak jak chcę, nie wiem czy nie trzeba będzie uciąć kawałka ścieżki
     list_of_frames = deep_list_of_frames.copy()
     deep_list_of_frames = []
     thr += thr_grow
@@ -217,37 +218,28 @@ for item in list_of_frames:
     print(item.offset)
 
 
-# Algorytm sprawdzania działa, bo sprawdzałem i dawało dobre rezultaty ale teraz się jebie przez inny tile size
-# tile sizy wychodzące poniżej w div_w i div_h są pojebane i kompletnie nie pasują do rzeczywistych tileów
-# + jescze nie mam pojęcia czy to wynikowe co wychodzi jest dobre, chyba że zły obraz wziąłem idk
-# do omówienia jutro (02.06), a na razie pushuje
-
-Image1 = Image.open("dobra_wycieta.png")
+Image1 = Image.open(source_image)
 Image1copy = Image1.copy()
 
 
 w_tile = abs(list_of_frames[0].offset[0] - list_of_frames[0].offset[2])
 h_tile = abs(list_of_frames[0].offset[1] - list_of_frames[0].offset[3])
 
-print("Tile width",w_tile)
-print("Tile height",h_tile)
-value = [255,255,255]
+print("Tile width", w_tile)
+print("Tile height", h_tile)
+value = [255, 255, 255]
 for square in list_of_frames:
     pos = []
     for item in list_of_frames:
-        flag = []
-        flag = check_adjacent(w_tile,h_tile,square.offset[0],item.offset[0],square.offset[1],item.offset[1])
-        #print("Square_offset x : ",square.offset[0])
-        #print("Square offset y : ",square.offset[1])
-        #print("item_offset x : ", item.offset[0])
-        #print("item offset y : ", item.offset[1])
+        flag = []  # DO WYWALENIA TA LINIA?
+        flag = check_adjacent(w_tile, h_tile, square.offset[0], item.offset[0], square.offset[1], item.offset[1])
         if flag[0] != '0':
             pos.append(flag[1])
-            if(flag[2]!='0'):
+            if flag[2]!='0':
                 pos.append(flag[3])
-        elif (flag[0] == '0' and flag[1] !='0'):
+        elif flag[0] == '0' and flag[1] !='0':
             pos.append(flag[2])
-    print("pos flag :",pos)
+    print("pos flag :", pos)
     border_right = 5
     border_left = 5
     border_top = 5
@@ -262,14 +254,16 @@ for square in list_of_frames:
     if 'B' in pos:
          border_bottom = 1
 
-    src = cv.imread(square.tilefname, cv.IMREAD_COLOR)
-    dst = cv.copyMakeBorder(src, border_top, border_bottom, border_left, border_right, cv.BORDER_CONSTANT, None,value)
-    cv.imwrite(square.tilefname, dst)
-    temp_img = Image.open(square.tilefname)
-    Image1copy.paste(temp_img, (square.offset[0] - border_left ,square.offset[1] - border_top))
-    Image1copy.save("final_board.png")
+# # Here is code that takes it's time, need to change it for some kind of grid
 
-Image1copy.show()
+#     src = cv.imread(square.tilefname, cv.IMREAD_COLOR)
+#     dst = cv.copyMakeBorder(src, border_top, border_bottom, border_left, border_right, cv.BORDER_CONSTANT, None,value)
+#     cv.imwrite(square.tilefname, dst)
+#     temp_img = Image.open(square.tilefname)
+#     Image1copy.paste(temp_img, (square.offset[0] - border_left ,square.offset[1] - border_top))
+#     Image1copy.save("final_board.png")
+#
+# Image1copy.show()
 
 
 
